9c4ca1b90acc32945b17f0c68717f2331e3d4120
Kay Sievers <kay@vrfy.org>
2021-02-09 18:01:17 +0100

    Import

---
+++ src/V2Color.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace V2Color {
+enum Hue {
+  Red     = 0,
+  Orange  = 30,
+  Yellow  = 60,
+  Green   = 120,
+  Cyan    = 180,
+  Blue    = 240,
+  Magenta = 300,
+};
+
+// CIE 1931 - psychometric lightness / human color vision
+uint8_t toCIE1931(uint8_t v);
+
+// Hue, Saturation, Value
+void HSVtoRGB(float h, float s, float v, uint8_t &r, uint8_t &g, uint8_t &b);
+};
